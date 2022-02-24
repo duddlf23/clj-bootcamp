732e0506d196dff9bc20c88e8d02df4cd3b9a85b
Youngil Choi <youngil@Youngilui-MacBookPro.local>
2022-02-24 16:59:33 +0900

    Fix day2 part2 & add day3 part 1

--- src/aoc2018/day2.clj
+++ src/aoc2018/day2.clj
@@ -22,42 +22,48 @@
          (map get-freq-nums-set)
          checksum-freq-filters
          (map count)
-         (reduce *))
-    )
-  )
+         (reduce *))))
+
 
 (comment
-  (get-checksum input)
-  )
+  (get-freq-nums-set (nth input 0))
+  (get-checksum input))
 
 
+((partial filter odd?) [1 3])
 
 
-
-
+((filter odd?) [1 3])
 
 ; Part 2
-(defn count-diff [xs ys]
-  (->> ys
-      (map = xs)
-      (filter false?)
-      count))
+(defn count-diff
+  "두 스트링의 각 문자를 순서대로 비교해 서로 다른 문자의 개수"
+  [xs ys]
+  (->> (map not= xs ys)
+       (filter true?)
+       count))
 
-(defn get-lcs [xs ys]
-  (->> ys
-       (map (fn [x y]
-              (if (= x y)
-                x
-                ""))
-            xs)
+(defn get-longest-matched-str
+  "두 스트링의 각 문자를 순서대로 비교해 서로 같은 모든 문자들의 부분 스트링"
+  [xs ys]
+  (->> (map vector xs ys)
+       (keep (fn [[x y]]
+               (when (= x y) x)))
        string/join))
 
-(reduce (fn [accumulated current]
-          (let [target
-                (->> accumulated
-                     (some #(when (= 1 (count-diff current %)) %)))]
-            (if (nil? target)
-              (conj accumulated current)
-              (reduced (get-lcs current target)))))
-        []
-        input)
+(defn find-common-letters [box-ids]
+  (reduce (fn [accumulated current-id]
+            (let [target-id
+                  (->> accumulated
+                       (some #(when (= 1 (count-diff current-id %)) %)))]
+              (if (nil? target-id)
+                (conj accumulated current-id)
+                (reduced (get-longest-matched-str current-id target-id)))))
+          []
+          box-ids))
+
+(comment
+  (count-diff "abb" "aaa")
+  (get-longest-matched-str "aaa" "aba")
+  (find-common-letters input))
+
---
+++ src/aoc2018/day3.clj
@@ -0,0 +1,60 @@
+(ns aoc2018.day3
+  "https://adventofcode.com/2018/day/3"
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+(def input-file "aoc2018/day3_in.txt")
+
+; part 1
+(defn parse-one-line-to-seq [line]
+  (->> line
+       (re-find #"@ (\d+),(\d+): (\d+)x(\d+)")
+       (drop 1)
+       (map edn/read-string)))
+
+(defn parse-one-line-to-hash-map [line]
+  (->> line
+       parse-one-line-to-seq
+       ((fn [[start-x start-y length-x length-y]]
+          {:start-x start-x
+           :start-y start-y
+           :length-x length-x
+           :length-y length-y}))))
+
+(comment
+  (parse-one-line-to-seq "#1 @ 12,548: 19x10")
+  (parse-one-line-to-hash-map "#1 @ 12,548: 19x10"))
+
+(defn get-input-xy-seq [file-name]
+  (->> (io/resource file-name)
+       io/reader
+       line-seq
+       (map parse-one-line-to-hash-map)))
+
+(defn get-all-fabric-coordinate-seq [file-name]
+  (let [get-fabric-coordinates (fn [{:keys [start-x start-y length-x length-y]}]
+                                 (for [dx (range length-x)
+                                       dy (range length-y)]
+                                   [(+ start-x dx) (+ start-y dy)]))]
+    (->> file-name
+         get-input-xy-seq
+         (mapcat get-fabric-coordinates))))
+
+(defn get-overlap-count [fabric-coordinates]
+  (->> fabric-coordinates
+       frequencies
+       vals
+       (filter #(> % 1))
+       count))
+
+(comment
+  (->> (get-all-fabric-coordinate-seq input-file)
+       get-overlap-count))
+
+
+(comment
+  (for [dx (range 5)
+        dy (range 6)]
+    [(+ dx 3) (+ dy 5)])
+  (get-input-xy-seq "aoc2018/day3_in_test.txt")
+  (get-all-fabric-coordinate-seq "aoc2018/day3_in_test.txt"))
