2adac98ff776ce06364b960998f9227b340ba3cd
Youngil Choi <youngil.choi@greenlabs.co.kr>
2022-03-07 02:22:19 +0900

    함수들을 도메인에 맞는 이름으로 변경

--- src/aoc2018/day5.clj
+++ src/aoc2018/day5.clj
@@ -1,72 +1,53 @@
 (ns aoc2018.day5
   "https://adventofcode.com/2018/day/5"
-  (:require [util.file :as file]
-            [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [util.file :as file]))
 
 (def input-file "aoc2018/day5_in.edn")
 (def input (file/read-edn input-file))
 
-(def upper-case-letters (map char (range 65 91)))
-(def lower-case-letters (map char (range 97 123)))
+(def upper-units (seq "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+(def lower-units (seq "abcdefghijklmnopqrstuvwxyz"))
 
-(def upper->lower (zipmap upper-case-letters lower-case-letters))
-(def lower->upper (zipmap lower-case-letters upper-case-letters))
-(def change-case-map (merge upper->lower lower->upper))
-
-; change-letter-case-map을 직접 함수로 써도 되나 해시 맵을 데이터라고 봤을 때 get을 쓰는게 좀 더 자연스럽다..?
-(defn change-case [letter]
-  (get change-case-map letter))
+(def upper->lower (zipmap upper-units lower-units))
+(def lower->upper (zipmap lower-units upper-units))
+(def change-polarity (merge upper->lower lower->upper))
 
 ; Part 1
 ; react
-(defn react [acc-polymers new-polymer]
-  (if (= (change-case new-polymer) (peek acc-polymers))
-    (pop acc-polymers)
-    (conj acc-polymers new-polymer)))
+(defn react [reacted new-polymer]
+  (if (= (change-polarity new-polymer) (peek reacted))
+    (pop reacted)
+    (conj reacted new-polymer)))
 
-(defn alchemical-reduce [polymers-str]
+(defn alchemical-reduce [polymers]
   (reduce react
           []
-          polymers-str))
-
-(defn alchemical-reduce-with-atom
-  "reduce를 안 써보려 하다가 등장한 괴물이다.
-  문제 특성상 reduce 과정이 fold-left의 형식이므로 병렬성도 보장이 안될텐데 atom을 써서 얻을 수 있는 이점이 잘 안 보인다.
-  그냥 써본 정도인가..?"
-  [polymers-str]
-  (let [reduced (atom [])]
-    (run!
-      (partial swap! reduced react)
-      polymers-str)
-    @reduced))
+          polymers))
 
 (comment
-  (= (last [\a \b]) (change-case \B))
+  (= (last [\a \b]) (change-polarity \B))
   (react [\a \e \f] \F)
   (alchemical-reduce "dabAcCaCBAcCcaDA")
+
   (-> input
       alchemical-reduce
-      count)
-  (-> input
-      alchemical-reduce-with-atom
       count))
 
 ; Part 2
-(defn get-distinct-letters-case-insensitive [s]
-  (-> s
+(defn get-distinct-units [polymers]
+  (-> polymers
       str/lower-case
       distinct))
 
-(defn remove-letter-case-insensitive [s letter]
-  (->> s
-       (remove #{letter (change-case letter)})
-       (apply str)))
+(defn remove-unit [polymers unit]
+  (remove #{unit (change-polarity unit)} polymers))
 
 (comment
-  (get-distinct-letters-case-insensitive "AdeAEdbFyh")
-  (remove-letter-case-insensitive "AdeAEdbFyh" \a)
+  (get-distinct-units "AdeAEdbFyh")
+  (remove-unit "AdeAEdbFyh" \a)
 
-  (->> (get-distinct-letters-case-insensitive input)
-       (map (partial remove-letter-case-insensitive input))
+  (->> (get-distinct-units input)
+       (map (partial remove-unit input))
        (map (comp count alchemical-reduce))
        (apply min)))
