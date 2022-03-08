275e7256a98322efc0be368f8b33ce84c353f193
Youngil Choi <youngil.choi@greenlabs.co.kr>
2022-03-08 16:32:21 +0900

    docstring 추가

--- src/aoc2020/day1.clj
+++ src/aoc2020/day1.clj
@@ -6,7 +6,9 @@
 
 ; Part 1
 
-(defn find-two-sum [nums-set target-sum]
+(defn find-two-sum
+  "수 하나를 잡고 구하고 싶은 총합에서 그 수를 뺀 값이 수의 집합에 속해있는지 확인해 만족하는 두 수의 쌍을 찾는다."
+  [nums-set target-sum]
   (-> (for [num nums-set
             :let [required-2nd-num (- target-sum num)]
             :when ((disj nums-set num) required-2nd-num)]
@@ -19,7 +21,9 @@
 
 ; Part 2
 
-(defn find-three-sum [nums-set target-sum]
+(defn find-three-sum
+  "수 하나를 잡고 구하고 싶은 총 합에서 그 수를 뺀 값의 two-sum이 가능한지 확인해 세 수의 쌍을 찾는다."
+  [nums-set target-sum]
   (-> (for [num nums-set
             :let [remain-two-sum (find-two-sum (disj nums-set num) (- target-sum num))]
             :when remain-two-sum]
